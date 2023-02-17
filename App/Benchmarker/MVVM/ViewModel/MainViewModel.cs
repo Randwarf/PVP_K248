56e3f53cf5a8ae7f8b8e8fbef8e0abf905d3acd6
Randwarf <matas.vaitkevicius@gmail.com>
2023-02-17 11:57:51 +0200

    Plain app view

---
+++ App/Benchmarker/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Benchmarker.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benchmarker.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public MainViewModel() 
+        {
+            
+        }
+    }
+}
