e398265e2adeee48bc791e67bd9b740362e75307
devJUKI <49684752+devJUKI@users.noreply.github.com>
2023-02-20 23:24:29 +0200

    Updated response codes

--- Server/main.py
+++ Server/main.py
@@ -5,18 +5,22 @@ database = []
 
 @app.route("/save-benchmark", methods=["POST"])
 def save_benchmark():
+    content_type = request.headers.get('Content-Type')
+    if "application/json" not in content_type:
+        return jsonify({"code": "415", "message": "Unsupported media type"}), 415
+
     args_json = request.json
     required_args = ["cpu", "energy", "ram"]
     benchmark_object = {}
 
     for arg in required_args:
         if arg not in args_json:
-            return jsonify({"code": "422", "message": "Not enough parameters"})
+            return jsonify({"code": "422", "message": "Not enough parameters"}), 422
 
         benchmark_object[arg] = args_json[arg]
 
     database.append(benchmark_object)
-    return jsonify({"code": "200", "message": "Benchmark uploaded sucessfully"})
+    return jsonify({"code": "200", "message": "Benchmark uploaded sucessfully"}), 200
 
 @app.route("/get-benchmark", methods=["GET"])
 def get_data():
