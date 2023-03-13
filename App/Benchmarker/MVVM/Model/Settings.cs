228c620c9a6a4346541b68f33e512433d977ae84
Randwarf <41196028+Randwarf@users.noreply.github.com>
2023-03-13 18:29:50 +0200

    IO settings

---
+++ App/Benchmarker/MVVM/Model/Settings.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benchmarker.MVVM.Model
+{
+    internal class Settings
+    {
+        [JsonProperty("agreedToDataSharing")]
+        public bool agreedToDataSharing = false;
+    }
+}
---
+++ App/Benchmarker/MVVM/Model/UserInfo.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace Benchmarker.MVVM.Model
+{
+    internal class UserInfo
+    {
+        public Settings settings;
+        private string path;
+
+        public UserInfo()
+        {
+            //TODO: Gal visą šitą path ištraukimą ir sukurimą iškelti į atskirą static klasę? 
+            //Dubliuojasi ir History.cs'e
+            path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/Benchmarker";
+            DirectoryInfo directory = new DirectoryInfo(path);
+            directory.Create();
+            path += "/Settings.json";
+            try //reading actual settings
+            {
+                using(StreamReader reader = new StreamReader(path))
+                {
+                    string content = reader.ReadToEnd();
+                    settings = JsonConvert.DeserializeObject<Settings>(content);
+                }
+            }
+            catch
+            {
+                //Something went wrong - use default settings
+                settings = new Settings();
+            }
+        }
+
+        public void saveSettings()
+        {
+            string content = JsonConvert.SerializeObject(settings);
+            using(StreamWriter writer = new StreamWriter(path, false))
+            {
+                writer.Write(content);
+            }
+        }
+    }
+}
