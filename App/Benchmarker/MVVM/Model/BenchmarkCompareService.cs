35b4fcda897b10b1c312b51fc495bcc553431104
devJUKI <49684752+devJUKI@users.noreply.github.com>
2023-03-21 16:39:08 +0200

    Benchmark comparison

---
+++ App/Benchmarker/MVVM/Model/BenchmarkCompareService.cs
@@ -0,0 +1,63 @@
+﻿using Benchmarker.MVVM.Model.DTOs;
+using Benchmarker.MVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+
+namespace Benchmarker.MVVM.Model
+{
+    public static class BenchmarkCompareService
+    {
+        public static List<ComparisonRow> CompareBenchmarks(HistoryBenchmark benchmark1, HistoryBenchmark benchmark2)
+        {
+            var row = new ComparisonRow()
+            {
+                Attribute = "Name",
+                Process1 = benchmark1.Process,
+                Process2 = benchmark2.Process
+            };
+
+            var row1 = CompareValues("CPU", benchmark1.CPU, benchmark2.CPU);
+            var row2 = CompareValues("RAM", benchmark1.RAM, benchmark2.RAM);
+            var row3 = CompareValues("Disk", benchmark1.Disk, benchmark2.Disk);
+            var row4 = CompareValues("Energy", benchmark1.Energy, benchmark2.Energy);
+
+            var rows = new List<ComparisonRow>() { row, row1, row2, row3, row4 };
+
+            return rows;
+        }
+
+        private static ComparisonRow CompareValues(string attributeName, double value1, double value2)
+        {
+            List<string> comparison = new List<string>() {
+                value1.ToString(),
+                value2.ToString()
+            };
+
+            if (value1 == value2)
+            {
+                return new ComparisonRow()
+                {
+                    Attribute = attributeName,
+                    Process1 = comparison[0],
+                    Process2 = comparison[1]
+                };
+            }
+
+            double minMetric = Math.Min(value1, value2);
+            double maxMetric = Math.Max(value1, value2);
+            double percentage = (minMetric != 0) ? Math.Abs(maxMetric - minMetric) / minMetric * 100 : 100;
+
+            int modifyIndex = percentage > 0 ? 0 : 1;
+            comparison[modifyIndex] += $" (+{Math.Round((percentage > 0 ? 1 : -1) * percentage, 2)})";
+
+            var row = new ComparisonRow()
+            {
+                Attribute = attributeName,
+                Process1 = comparison[0],
+                Process2 = comparison[1]
+            };
+
+            return row;
+        }
+    }
+}
--- App/Benchmarker/MVVM/ViewModel/CompareViewModel.cs
+++ App/Benchmarker/MVVM/ViewModel/CompareViewModel.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using Benchmarker.Core;
+using Benchmarker.MVVM.Model;
+using Benchmarker.MVVM.Model.DTOs;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Benchmarker.MVVM.ViewModel
 {
-    internal class CompareViewModel
+    internal class CompareViewModel : ObservableObject
     {
+        private List<ComparisonRow> benchmarkComparisonRows;
+
+        public List<ComparisonRow> BenchmarkComparisonRows
+        {
+            get { return benchmarkComparisonRows; }
+            set { benchmarkComparisonRows = value; OnPropertyChanged(); }
+        }
+
+        public CompareViewModel()
+        {
+            HistoryBenchmark benchmark1 = HistoryService.GetBenchmarks()[5];
+            HistoryBenchmark benchmark2 = HistoryService.GetBenchmarks()[6];
+            benchmarkComparisonRows = BenchmarkCompareService.CompareBenchmarks(benchmark1, benchmark2);
+            
+        }
+    }
+
+    public class ComparisonRow
+    {
+        public string Attribute { get; set; }
+        public string Process1 { get; set; }
+        public string Process2 { get; set; }
     }
 }
