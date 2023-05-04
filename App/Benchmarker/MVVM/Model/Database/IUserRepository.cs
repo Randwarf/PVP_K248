59f337710aa2eb180e6d6adcbb89ea19f89a5d5d
Randwarf <matas.vaitkevicius@gmail.com>
2023-05-04 15:53:55 +0300

    tokenai appse

--- App/Benchmarker/MVVM/Model/Database/IUserRepository.cs
+++ App/Benchmarker/MVVM/Model/Database/IUserRepository.cs
@@ -5,10 +5,12 @@ namespace Benchmarker.MVVM.Model.Database
 {
     internal interface IUserRepository
     {
-        Task<User> Login(User user);
+        Task<string> Login(User user);
+        void Logout(string token);
         Task<List<User>> GetAllUsers();
         Task<User> GetUserById(int id);
         Task<User> GetUserByEmail(string email);
+        Task<User> GetUserByToken(string token);
         void InsertUser(User benchmark);
         void UpdateUser(User benchmark);
         void DeleteUser(int id);
