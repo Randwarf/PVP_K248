08a4218eb1edac3fd8c75ffbff7db7b8a1f8794b
devJUKI <49684752+devJUKI@users.noreply.github.com>
2023-05-18 23:04:42 +0300

    Working color coding

--- App/Benchmarker/MVVM/Model/DTOs/HistoryBenchmark.cs
+++ App/Benchmarker/MVVM/Model/DTOs/HistoryBenchmark.cs
@@ -1,33 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Windows.Media;
 
 namespace Benchmarker.MVVM.Model.DTOs
 {
-    public class HistoryBenchmark
-    {
-        private Guid Id { get; set; }
-        public DateTime Date { get; set; }
-        public double CPU { get; set; }
-        public double RAM { get; set; }
-        public double Energy { get; set; }
-        public string Process { get; set; }
-        public double Disk { get; set; }
+	public class HistoryBenchmark
+	{
+		private Guid Id { get; set; }
+		public DateTime Date { get; set; }
+		public double CPU { get; set; }
+		public double RAM { get; set; }
+		public double Energy { get; set; }
+		public string Process { get; set; }
+		public double Disk { get; set; }
+		public SolidColorBrush RowColor { get; set; } = Brushes.Pink;
 
-        public HistoryBenchmark() { }
+		public HistoryBenchmark() { }
 
-        public HistoryBenchmark(Benchmark benchmark)
-        {
-            Id = Guid.NewGuid();
-            Date = benchmark.Date;
-            CPU = benchmark.CPU;
-            RAM = benchmark.RAM;
-            Energy = benchmark.Energy;
-            Process = benchmark.Process;
-            Disk = benchmark.Disk;
-        }
+		public HistoryBenchmark(Benchmark benchmark)
+		{
+			Id = Guid.NewGuid();
+			Date = benchmark.Date;
+			CPU = benchmark.CPU;
+			RAM = benchmark.RAM;
+			Energy = benchmark.Energy;
+			Process = benchmark.Process;
+			Disk = benchmark.Disk;
+			RowColor = Brushes.White;
+		}
 
-        public Guid GetId()
-        {
-            return Id;
-        }
-    }
+		public Guid GetId()
+		{
+			return Id;
+		}
+	}
 }
--- App/Benchmarker/MVVM/ViewModel/HistoryViewModel.cs
+++ App/Benchmarker/MVVM/ViewModel/HistoryViewModel.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Windows.Media;
 using Benchmarker.Core;
 using Benchmarker.MVVM.Model;
 using Benchmarker.MVVM.Model.DTOs;
@@ -35,8 +37,30 @@ namespace Benchmarker.MVVM.ViewModel
         private void UpdateTable()
         {
             Benchmarks = null;
-            Benchmarks = HistoryService.GetBenchmarks();
+            var historyBenchmarks = HistoryService.GetBenchmarks();
+            var coloredBenchmarks = SetRowColorBasedOnEnergy(historyBenchmarks);
+            coloredBenchmarks.Sort((x, y) => y.Energy.CompareTo(x.Energy));
+            Benchmarks = coloredBenchmarks;
             benchmarkViewModel.StartVM.ResetScreen();
         }
-    }
+
+		private List<HistoryBenchmark> SetRowColorBasedOnEnergy(List<HistoryBenchmark> benchmarks)
+		{
+            double maxEnergy = benchmarks.Select(x => x.Energy).Max();
+
+			foreach (HistoryBenchmark benchmark in benchmarks)
+			{
+				double normalizedEnergy = benchmark.Energy / maxEnergy;
+				byte red = (byte)(255 * (1 - normalizedEnergy));
+				byte green = (byte)(255 * normalizedEnergy);
+				byte blue = 0;
+
+				Color color = Color.FromRgb(red, green, blue);
+				SolidColorBrush rowColor = new SolidColorBrush(color);
+				benchmark.RowColor = rowColor;
+			}
+
+            return benchmarks;
+		}
+	}
 }
